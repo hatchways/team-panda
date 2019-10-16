72b4bf0752d40573f72659140bc5256ac468a660
Amauris Ferreira <ar.ferreira17@gmail.com>
2019-10-16 17:08:33 -0400

    add validation to sequelize user model

--- server/models/user.js
+++ server/models/user.js
@@ -13,14 +13,45 @@ const user = (sequelize, DataTypes) => {
     },
     email: {
       type: DataTypes.STRING,
-      unique: true,
-      allowNull: false
+      unique: {
+        args: true,
+        msg: "This email is already taken. Please try another one."
+      },
+      allowNull: false,
+      validate: {
+        isEmail: {
+          args: true,
+          msg: "Email is invalid."
+        }
+      }
     },
     password: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        len: {
+          args: [6],
+          msg: "Password must be at least 6 characters."
+        }
+      },
+      scopes: false || [] 
+    },
+    confirmPassword: {
+      type: DataTypes.VIRTUAL,
+      allowNull: false,
+      validate: {
+        passwordsMatch: function(val){
+          if(val !== this.get('password')){
+            throw new Error("Passwords must match.");
+          }
+        }
+      },
+      scopes: false || []
     }
   }, {
+    defaultScope: {
+      attributes: {exclude: ['password','currentPassword']}
+    },
     hooks : {
       afterValidate: (usr, options) => {
         return hashPass(usr);
