b08b5fde64c704f935e97a901b0175b70091d085
Andy Poon <anpoon430@gmail.com>
2019-10-19 15:14:37 -0400

    added basic auth utils WIP

---
+++ client/src/utils/auth.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+export const authLogin = async() => {
+    //WIP depending on what the login route returns
+    const res = await axios.post('/login');
+    return res;
+}
+
+export const authSignUp = async() => {
+    //WIP depending on what the signup route returns
+    const res = await axios.post('/signup');
+    return res;
+}
+
+export const authLogOut = async() => {
+    //WIP depending on what the logout route returns
+    const res = await axios.post('/logout');
+    return res;
+}
+
