120b6dd2669bb5e6a9315f4bc930cfabc5a88a62
Andy Poon <anpoon430@gmail.com>
2019-10-19 14:04:38 -0400

    auth provider component

---
+++ client/src/utils/AuthProvider.js
@@ -0,0 +1,46 @@
+import React, {createContext, useState, useContext} from 'react';
+import axios from 'axios';
+import { authLogIn, authSignUp, authLogOut } from './auth';
+
+const AuthContext = createContext();
+
+//provider component to wrap app and creates an auth object that can be
+//consumed in any child using useAuth hook
+export default function AuthProvider({children}){
+    const auth = useProvideAuth();
+    return <AuthContext.Provider value = {auth}>{children}</AuthContext.Provider>
+}
+
+//hook for child component to get access to auth object
+export const useAuth =() => {
+    return useContext(AuthContext);
+}
+
+//hook provider that creates auth object and handles user state
+function useProvideAuth(){
+    const [user, setUser] = useState(null);
+
+    const logIn = async(email, password) => {
+
+        const res = await authLogIn();
+        setUser(res.user);
+    };
+    const signUp = async(email, password) => {
+
+        const res = await authSignUp();
+        setUser(res.user);
+    };
+    const signOut = async(email, password) => {
+
+        const res = await authLogOut();
+        setUser(res.user);
+    };
+
+    return {
+        logIn,
+        signUp,
+        signOut,
+        user
+    }
+}
+
