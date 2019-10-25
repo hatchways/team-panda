74ac65c79b215ba427bc3f041501ed1b280c217a
Andy Poon <anpoon430@gmail.com>
2019-10-25 00:52:56 -0400

    show snackbar to display error message for server side validation

--- client/src/components/AuthForm.js
+++ client/src/components/AuthForm.js
@@ -1,145 +1,166 @@
-import React, {useState, useEffect, useContext, useReducer} from 'react'
-import PropTypes from 'prop-types'
-import TextField from '@material-ui/core/TextField'
-import {makeStyles} from '@material-ui/core/styles'
-import {Button} from '@material-ui/core'
-import { useAuth } from '../utils/AuthProvider'
-import _ from 'lodash';
+import React, { useState, useRef } from "react";
+import PropTypes from "prop-types";
+import TextField from "@material-ui/core/TextField";
+import { makeStyles } from "@material-ui/core/styles";
+import { Button } from "@material-ui/core";
+import { useAuth } from "../utils/AuthProvider";
+import _ from "lodash";
 const useStyles = makeStyles(theme => ({
-    root: {
+    root: {},
+    container: {},
+    textField: {},
 
-    },
-    container: {
-
-    },
-    textField: {
-
-    },
-
-    displayName: {
-
-    }
-
+    displayName: {}
 }));
 
-
 const AuthForm = props => {
-    const {name, displayName} = props
+    const { name, displayName } = props;
     const classes = useStyles();
-    const [authForm, setAuthForm] = useState({email: '', password: '', name: '', repeatPassword: '', errors: null});
+    const [authForm, setAuthForm] = useState({
+        email: "",
+        password: "",
+        name: "",
+        confirmPassword: ""
+    });
+
+    const pwdRef = useRef(null);
+    const pwdRepeatRef = useRef(null);
     const handleChange = elName => event => {
-        if (name === 'signup'){
-            if (elName === 'repeatPassword'){
-                if (authForm.password !== event.target.value){
-                    event.target.setCustomValidity('The passwords you have entered do not match');
-                } else {
-                    event.target.setCustomValidity('');
-                }
-            }
-            else if (elName === 'password' && event.target.value && event.target.value.length < 6){
-                event.target.setCustomValidity('Passwords have to be atleast 6 characters long');
+        if (name === "signup") {
+            if (
+                (elName === "password" || elName === "confirmPassword") &&
+                event.target.value &&
+                event.target.value.length < 6
+            ) {
+                event.target.setCustomValidity(
+                    "Passwords have to be atleast 6 characters long"
+                );
+                setAuthForm({ ...authForm, [elName]: event.target.value });
+                return;
             } else {
-                event.target.setCustomValidity('');
+                event.target.setCustomValidity("");
+            }
+            if (elName === "confirmPassword") {
+                if (authForm.password !== event.target.value) {
+                    event.target.setCustomValidity(
+                        "The passwords you have entered do not match"
+                    );
+                    setAuthForm({ ...authForm, [elName]: event.target.value });
+                    return;
+                } else {
+                    event.target.setCustomValidity("");
+                }
             }
         }
         setAuthForm({ ...authForm, [elName]: event.target.value });
-      };
+    };
 
-    const {
-        logIn,
-        user,
-        signUp,
-        logOut
-    } = useAuth();
+    const { logIn, user, signUp, logOut } = useAuth();
 
-
-    const handleSubmit = (event) => {
+    const handleSubmit = event => {
         event.preventDefault();
-        switch(name){
-            case 'login':
-                logIn(_.pick(authForm, ['email', 'password']));
+        switch (name) {
+            case "login":
+                logIn(_.pick(authForm, ["email", "password"]));
                 break;
-            case 'signup':
-                signUp(_.pick(authForm, ['name', 'email', 'password']))
+            case "signup":
+                signUp(authForm);
+                break;
+            default:
+                return;
         }
-    }
+    };
 
     return (
-      <div>
-        <h1 className = {classes.displayName}>{name === 'signup' ? 'Create an Account' : displayName}</h1>
-        <form
-          className={classes.container}
-          onSubmit={handleSubmit}
-          name={name}
-          autoComplete="off"
-        >
-          <div>
-            {name === 'signup' ?
-                <TextField
-                id="name"
-                label="Name"
-                className={classes.textField}
-                margin="normal"
-                value = {authForm.name}
-                onChange = {handleChange('name')}
-                variant = 'outlined'
-                required
-                fullWidth
-                />: ''}
-            <TextField
-              id="email"
-              label="Email"
-              type = 'email'
-              className={classes.textField}
-              margin="normal"
-              value = {authForm.email}
-              onChange = {handleChange('email')}
-              required
-              variant = 'outlined'
-              fullWidth
-            />
-          </div>
-          <div>
-            <TextField
-              id="password"
-              label="Password"
-              className={classes.textField}
-              margin="normal"
-              type="password"
-              value = {authForm.password}
-              onChange = {handleChange('password')}
-              required
-              variant = 'outlined'
-              fullWidth
-            />
-            {name === 'signup' ?
-             <TextField
-             id="repeatpassword"
-             label="Repeat Password"
-             className={classes.textField}
-             margin="normal"
-             type="password"
-             value = {authForm.repeatPassword}
-             onChange = {handleChange('repeatPassword')}
-             required
-             variant = 'outlined'
-             fullWidth
+        <div>
+            <h1 className={classes.displayName}>
+                {name === "signup" ? "Create an Account" : displayName}
+            </h1>
+            <form
+                className={classes.container}
+                onSubmit={handleSubmit}
+                name={name}
+                autoComplete="off"
+            >
+                <div>
+                    {name === "signup" ? (
+                        <TextField
+                            id="name"
+                            label="Name"
+                            className={classes.textField}
+                            margin="normal"
+                            value={authForm.name}
+                            onChange={handleChange("name")}
+                            variant="outlined"
+                            required
+                            fullWidth
+                        />
+                    ) : (
+                        ""
+                    )}
+                    <TextField
+                        id="email"
+                        label="Email"
+                        type="email"
+                        className={classes.textField}
+                        margin="normal"
+                        value={authForm.email}
+                        onChange={handleChange("email")}
+                        required
+                        variant="outlined"
+                        fullWidth
+                    />
+                </div>
+                <div>
+                    <TextField
+                        id="password"
+                        label="Password"
+                        className={classes.textField}
+                        margin="normal"
+                        type="password"
+                        value={authForm.password}
+                        onChange={handleChange("password")}
+                        required
+                        variant="outlined"
+                        fullWidth
+                        inputRef={pwdRef}
+                    />
+                    {name === "signup" ? (
+                        <TextField
+                            id="confirmPassword"
+                            label="Repeat Password"
+                            className={classes.textField}
+                            margin="normal"
+                            type="password"
+                            value={authForm.confirmPassword}
+                            onChange={handleChange("confirmPassword")}
+                            required
+                            variant="outlined"
+                            fullWidth
+                            inputRef={pwdRepeatRef}
+                        />
+                    ) : (
+                        ""
+                    )}
+                </div>
+                <div>
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        size="small"
+                        type="submit"
+                    >
+                        {name === "signup" ? "Create" : displayName}
+                    </Button>
+                </div>
+            </form>
+        </div>
+    );
+};
 
-           />: ''}
-          </div>
-          <div>
-            <Button variant="contained" color="primary" size="small" type="submit">
-              {name === 'signup'? 'Create': displayName}
-            </Button>
-          </div>
-        </form>
-      </div>
-    )
-}
+export default AuthForm;
 
-export default AuthForm
-
-  /**
+/**
  * PROP TYPES
  */
 
--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -1,29 +1,34 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import AuthForm from "../components/AuthForm";
 import { makeStyles } from "@material-ui/core/styles";
-import { Button } from "@material-ui/core";
+import { Button, Snackbar } from "@material-ui/core";
 import { Link } from "react-router-dom";
+import { useAuth } from "../utils/AuthProvider";
+import { MySnackbarContentWrapper } from "../components/Snackbar";
 
 const useStyles = makeStyles(theme => ({
     root: {
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        padding: 20,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        padding: 20
     },
     container: {
-        display: 'flex'
+        display: "flex"
     },
     authButton: {
-        width: '50vw',
-        display: 'flex',
-        justifyContent: 'flex-end'
+        width: "50vw",
+        display: "flex",
+        justifyContent: "flex-end"
     },
     authPicture: {
-        height: '70vh'
+        height: "70vh"
     },
-    authContainer:{
-        padding: '10px 50px 10px 10px'
+    authContainer: {
+        padding: "10px 50px 10px 10px"
+    },
+    margin: {
+        margin: theme.spacing(1)
     }
 }));
 
@@ -33,23 +38,59 @@ export default function AuthPage({ name, displayName }) {
         signup: "/login",
         login: "/signup"
     };
+    const { user, authError, setAuthError } = useAuth();
+    const [open, setOpen] = useState(false);
+
+    const handleOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        console.log('closing snackabr', open);
+        setAuthError(null);//clear auth error
+        setOpen(false);
+    };
+
+    useEffect(() => {
+        if (authError && !open) {
+            console.log('opening snackbar');
+            handleOpen();
+        }
+    });
 
     return (
-        <div className = {classes.root}>
+        <div className={classes.root}>
             <div className={classes.container}>
-                <img className = {classes.authPicture} src="authPicture.png" alt="two dogs" />
+                <img
+                    className={classes.authPicture}
+                    src="authPicture.png"
+                    alt="two dogs"
+                />
                 <div className={classes.authContainer}>
                     <div className={classes.authButton}>
                         <Link to={mapRoutes[name]}>
-                            <Button variant = 'outlined'>
+                            <Button variant="outlined">
                                 {name === "signup" ? "Log In" : "Sign Up"}
                                 {/* show login button on sign up page and sign up button on log in page for switching */}
                             </Button>
                         </Link>
                     </div>
-                    <AuthForm name={name} displayName = {displayName}/>
+                    <AuthForm name={name} displayName={displayName} />
                 </div>
             </div>
+            <Snackbar
+                anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+                open={open}
+                onClose={handleClose}
+                autoHideDuration = {5000}
+            >
+                <MySnackbarContentWrapper
+                    variant="error"
+                    className={classes.margin}
+                    onClose = {handleClose}
+                    message={(authError && authError.data && authError.data.errorMsg) || ''}
+                />
+            </Snackbar>
         </div>
     );
 }
