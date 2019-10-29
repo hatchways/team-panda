d7378d9eee8f6406e8a24abb66a42c84241a215b
Amauris Ferreira <ar.ferreira17@gmail.com>
2019-10-28 20:14:34 -0400

    add UserProfile routes, add passport strategies

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,49 +3,71 @@ var router = express.Router();
 const models = require('../models').default;
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const { AuthenticationError } = require('../errors');
 
-router.post('/register', (req, res, next) => {
-  let newUser = {
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password,
-    confirmPassword: req.body.confirmPassword
-  }
-  models.User.create(newUser)
-    .then((user) => {
-      res.status(201).json(createJWTResponse(user));
+module.exports = (auth) => {
+  router.post('/register', (req, res, next) => {
+    let newUser = {
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+      confirmPassword: req.body.confirmPassword
+    }
+    models.User.create(newUser)
+      .then((user) => {
+        res.status(201).json(createJWTResponse(user));
+      }).catch(next);
+  });
+  
+  router.post('/login', (req, res, next) => {
+    let user_cred = {
+      email: req.body.email,
+      password: req.body.password,
+    }
+    models.User.findOne({where: {email: user_cred.email}}).then((user)=> {
+        return validateLogin(user, user_cred);
+      }).then((user) => {
+        res.status(200).json(createJWTResponse(user));
+      }).catch(next);
+  });
+  
+  router.post('/:userId', auth.authenticate('jwt-param-id', { session: false }), (req, res, next) => {
+    let userProfile = req.body;
+    userProfile.userId = req.params.userId;
+    models.UserProfile.create(userProfile).then((userProfile) => {
+      res.status(201).send(userProfile);
+      // SequelizeForeignKeyConstraintError
     }).catch(next);
-});
+  });
 
-router.post('/login', (req, res, next) => {
-  let user_cred = {
-    email: req.body.email,
-    password: req.body.password,
-  }
-  models.User.findOne({where: {email: user_cred.email}}).then((user)=> {
-      return validateLogin(user, user_cred);
-    }).then((user) => {
-      res.status(200).json(createJWTResponse(user));
+  router.put('/:userId', auth.authenticate('jwt-param-id', { session: false }), (req, res, next) => {
+    let updatedProfile = req.body;
+    updatedProfile.userId = req.params.userId;
+    models.UserProfile.updateById(updatedProfile.userId, updatedProfile).then(() => {
+      res.status(200).send({success: "User Profile was updated"});
     }).catch(next);
-});
+  });
 
-router.post('/:userId', (req, res, next) => {
-  let userProfile = req.body;
-  userProfile.userId = req.params.userId;
-  models.UserProfile.create(userProfile).then((userProfile) => {
-    res.send(userProfile);
-    // SequelizeForeignKeyConstraintError
-  }).catch(next);
-});
+  router.get('/:userId', auth.authenticate('jwt', { session: false}), (req, res, next) => {
+    models.UserProfile.findOne({where: {userId: req.params.userId}}).then((userProfile) => {
+      if(userProfile){
+        return res.status(200).send(userProfile);
+      }
+      res.status(404).send();
+    }).catch(next);
+  });
+
+  return router;
+};
 
 function validateLogin(actualUser, givenUser){
   if (actualUser){
     return bcrypt.compare(givenUser.password,actualUser.password).then((res) =>{
       if(res){ return Promise.resolve(actualUser) }
-        return Promise.reject({loginError: 'Incorrect username/password'})  
+        return Promise.reject(new AuthenticationError('Incorrect username/password'))  
     });
   }else {
-    return Promise.reject({loginError: 'Incorrect username/password'});  
+    return Promise.reject(new AuthenticationError('Incorrect username/password'));  
   }
 }
 
@@ -53,7 +75,6 @@ function createJWTResponse(user){
   return {
       "email": user.email,
       "id": user.id,
-      "token": jwt.sign(user.id, "tempSecret")
+      "token": jwt.sign({id: user.id}, "tempSecret")
     }
 }
-module.exports = router;
