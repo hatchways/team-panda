ae6b0de96ed36c2b6092f236cd3c4539a47f8dc6
Amauris Ferreira <ar.ferreira17@gmail.com>
2019-10-28 20:13:35 -0400

    create custom errors

---
+++ server/errors/ForbiddenError.js
@@ -0,0 +1,14 @@
+const CustomError = require('./CustomError') 
+
+class ForbiddenError extends CustomError{
+  constructor(message="Forbidden", ...params) {
+    super(...params);
+    if(Error.captureStackTrace){
+      Error.captureStackTrace(this, ForbiddenError);
+    }
+    this.status=403;
+    this.message=message;
+  }
+}
+
+module.exports = ForbiddenError;
