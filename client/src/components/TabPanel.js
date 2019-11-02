6e1c8382498c515d3aeb95ca77ca29a19fe523e9
Andy Poon <anpoon430@gmail.com>
2019-11-02 18:53:22 -0400

    tab panel from ui examples

---
+++ client/src/components/TabPanel.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Typography, Box } from '@material-ui/core';
+
+//From https://material-ui.com/components/tabs/#simple-tabs
+export default function TabPanel(props) {
+    const { children, value, index, ...other } = props;
+
+    return (
+      <Typography
+        component="div"
+        role="tabpanel"
+        hidden={value !== index}
+        id={`simple-tabpanel-${index}`}
+        aria-labelledby={`simple-tab-${index}`}
+        {...other}
+      >
+        <Box>{children}</Box>
+      </Typography>
+    );
+  }
+
+  TabPanel.propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.any.isRequired,
+    value: PropTypes.any.isRequired,
+  };
