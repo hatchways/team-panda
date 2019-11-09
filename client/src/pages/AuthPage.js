6b58317e8ed6610f231a23b2273241d201ae263d
Andy Poon <anpoon430@gmail.com>
2019-11-09 15:42:09 -0500

    restyled login/signup page

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -1,18 +1,20 @@
 import React from "react";
 import AuthForm from "../components/AuthForm";
 import { makeStyles } from "@material-ui/core/styles";
-import { Button, Typography } from "@material-ui/core";
-import LinkButton from "../components/LinkButton";
 
 const useStyles = makeStyles(theme => ({
     root: {
         display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        padding: 20
+        justifyContent: "space-around",
+        alignItems: "flex-start",
+        padding: 20,
+        background: "url(auth-picture.png) no-repeat left",
+        height: "calc(100vh - 64px)"
     },
     container: {
-        display: "flex"
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "flex-start"
     },
     authButton: {
         width: "50vw",
@@ -32,34 +34,11 @@ const useStyles = makeStyles(theme => ({
 
 export default function AuthPage({ name, displayName }) {
     const classes = useStyles();
-    const mapRoutes = {
-        signup: "/login",
-        login: "/signup"
-    };
 
     return (
         <div className={classes.root}>
             <div className={classes.container}>
-                <img
-                    className={classes.authPicture}
-                    src="auth-picture.png"
-                    alt="two dogs"
-                />
-                <div className={classes.authContainer}>
-                    <div className={classes.authButton}>
-                        <Button
-                            variant="outlined"
-                            component={LinkButton}
-                            to={mapRoutes[name]}
-                        >
-                            <Typography color="textPrimary" variant="button">
-                                {name === "signup" ? "Log In" : "Sign Up"}
-                            </Typography>
-                            {/* show login button on sign up page and sign up button on log in page for switching */}
-                        </Button>
-                    </div>
-                    <AuthForm formName={name} displayName={displayName} />
-                </div>
+                <AuthForm formName={name} displayName={displayName} />
             </div>
         </div>
     );
