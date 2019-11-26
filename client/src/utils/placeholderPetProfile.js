d7cf2f4c613a5330cb262d05ddd9679d2ff4899e
Amauris Ferreira <ar.ferreira17@gmail.com>
2019-11-25 22:56:06 -0500

    placeholder pet profile

---
+++ client/src/utils/placeholderPetProfile.js
@@ -0,0 +1,44 @@
+const placeholderPetProfile = {
+    name: "Meepers",
+    location: "Toronto, Canada",
+    ownerId: "1",
+    dateOfBirth: "2010-10-12",
+    animal: "Cat",
+    profilePic: "../../../meepers.png",
+    about: `I'm a cat that lives with my siblings Buster and Snoopy. They are dogs
+    but they are welcomed in our loving home! My owneris Chris and he takes care of us very well. My
+    favorite hobbies are napping and snacking. I am very calm and often like to chill in my little nook.`
+};
+
+const placeholderPosts = [
+    {
+        id: 1,
+        image: "../../../samplePost1.png",
+        message: "A day in the life of Meepers",
+        postedDate: "07/20/2019",
+        numLikes: 20
+    },
+    {
+        id: 2,
+        image: "../../../samplePost2.png",
+        message: "I'm getting pampered",
+        postedDate: "07/01/2019",
+        numLikes: 20
+    },
+    {
+        id: 3,
+        image: "../../../samplePost3.png",
+        message: "Naps",
+        postedDate: "07/01/2019",
+        numLikes: 20
+    },
+    {
+        id: 4,
+        image: "../../../samplePost4.png",
+        message: "Sleeping",
+        postedDate: "07/01/2019",
+        numLikes: 20
+    }
+];
+export default placeholderPetProfile;
+export { placeholderPosts };
