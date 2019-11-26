706ce9ab5da8adcb847920271b057d88dd0a712d
Amauris Ferreira <ar.ferreira17@gmail.com>
2019-11-25 23:05:51 -0500

    add postService, more petService methods to access more backend API routes

--- client/src/utils/petService.js
+++ client/src/utils/petService.js
@@ -1,5 +1,4 @@
 import { authRequest } from "../utils/auth";
-
 export const createPet = async (userId, petInfo) => {
     let formData = new FormData();
     for (let prop in petInfo) {
@@ -13,8 +12,40 @@ export const createPet = async (userId, petInfo) => {
             "Content-Type": "multipart/form-data"
         }
     });
-    if (res.status === 201) {
-        console.log(res.data);
-    }
     return res.data;
+};
+
+export const updatePet = async (userId, petId, petInfo) => {
+    let formData = new FormData();
+    for (let prop in { petId, ...petInfo }) {
+        if (petInfo[prop]) formData.append(prop, petInfo[prop]);
+    }
+    const res = await authRequest(`/users/${userId}/pets/${petId}/edit`, {
+        method: "PUT",
+        data: formData,
+        headers: {
+            Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+            "Content-Type": "multipart/form-data"
+        }
+    });
+    return res.data.profile;
+};
+
+export const getUsersPets = async userId => {
+    const res = await authRequest(`/users/${userId}/pets`, {
+        method: "GET"
+    });
+    if (res.status === 200) {
+        return res.data;
+    }
+};
+
+export const getPetWithPosts = async (userId, petId) => {
+    const res = await authRequest(`/users/${userId}/pets/${petId}`, {
+        method: "GET"
+    });
+    if (res.status === 200) {
+        return res.data;
+    }
+    return {};
 };
---
+++ client/src/utils/postService.js
@@ -0,0 +1,38 @@
+import { authRequest } from "./auth";
+export const createPost = async (userId, petId, postInfo) => {
+    let formData = new FormData();
+    for (let prop in postInfo) {
+        if (postInfo[prop]) formData.append(prop, postInfo[prop]);
+    }
+    const res = await authRequest(`/users/${userId}/pets/${petId}/posts/new`, {
+        method: "POST",
+        data: formData,
+        headers: {
+            Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+            "Content-Type": "multipart/form-data"
+        }
+    });
+    if (res.status === 201) {
+        console.log(res.data);
+    }
+    return res.data;
+};
+
+export const updatePost = async (userId, petId, postInfo) => {
+    let formData = new FormData();
+    for (let prop in { petId, ...postInfo }) {
+        if (postInfo[prop]) formData.append(prop, postInfo[prop]);
+    }
+    const res = await authRequest(
+        `/users/${userId}/pets/${petId}/posts/${postInfo.id}`,
+        {
+            method: "PUT",
+            data: formData,
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+                "Content-Type": "multipart/form-data"
+            }
+        }
+    );
+    return res.data;
+};
